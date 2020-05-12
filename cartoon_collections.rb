5f082fe434e3a13762f782617c25c2ea15fd0ff4
Tommy Werner <wtwerner@gmail.com>
2020-05-12 23:21:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+   i = 1
+  dwarves.each do |dwarf|
+    puts "#{i}. #{dwarf}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  calls_caps = []
+  planeteer_calls.each do |call|
+    calls_caps <<
+    "#{call.capitalize}!"
+  end
+  calls_caps
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
+      return true
+      end
+    else
+      return false
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
